697ca3e38627834e0b57199cf630dbc5c23fdb39
Al3J077 <garciaalejo711@gmail.com>
2025-11-21 08:23:16 +0000

    funcionará??

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rungo.app',   // identificador único de tu app
-  appName: 'run_rungo',     // nombre visible de la app
-  webDir: 'out'             // carpeta exportada por Next.js
+  appId: 'com.rungo.app',
+  appName: 'run_rungo',
+  webDir: 'out'
 };
 
 export default config;
