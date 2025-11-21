dfbc6fc10a881117b3d6ea6ab7adc4b7f3b522d6
Al3J077 <garciaalejo711@gmail.com>
2025-11-21 06:08:04 +0000

    gen_APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rungo.app',   // identificador único de tu app
+  appName: 'run_rungo',     // nombre visible de la app
+  webDir: 'out'             // carpeta exportada por Next.js
+};
+
+export default config;
